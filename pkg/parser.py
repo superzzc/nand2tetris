cb36be0171b030286a10e1bf244ec446af0eeee5
superzzc <1520594288@qq.com>
2025-10-09 17:28:27 +0800

    new file:   nosymbol-assemble.py 	new file:   pkg/code.py 	new file:   pkg/parser.py 	deleted:    code.py 	deleted:    parser.py

---
+++ pkg/parser.py
@@ -0,0 +1,97 @@
+import re
+class Parser():
+    a_command_pattern = r'^@(.+)$'
+    l_command_pattern = r'^\(([A-Z]+)\)$'
+    c_command_pattern = r'^(?:(\w+)=)?([\w\+\-\&\|\!]+)(?:;(\w+))?$'
+
+    def __init__(self,filename):
+        self.file = open(filename, 'r')
+        self.file.seek(0)
+        self.current_command = None
+        self.line_number = 0
+    
+    def hasMoreCommands(self):
+        '''
+        如果输入中还有更多的命令，则返回真，否则返回假
+        '''
+        current = self.file.tell()
+        self.file.seek(0, 2)  # 移动到文件末尾
+        end = self.file.tell()
+        self.file.seek(current)  # 恢复原来的位置
+        return current != end
+        
+    def advance(self):
+        '''
+        从输入中读取下一条命令，并使其成为当前命令
+        仅当hasMoreCommands()为真时调用
+        '''
+        if self.hasMoreCommands():
+            line = self.file.readline()
+            # 去除注释和空白
+            line = re.split(r'#|//', line)[0].strip()
+            while line == '' and self.hasMoreCommands():
+                line = self.file.readline()
+                line = re.split(r'#|//', line)[0].strip()
+            self.current_command = line
+            self.line_number += 1
+        else:
+            self.file.close()
+
+    def commandType(self):
+        '''
+        返回当前命令的类型
+        A_COMMAND, @xxx
+        C_COMMAND, dest=comp;jump/dest=comp/comp
+        L_COMMAND中的一个,(XXX) 例如(LOOP)
+        '''
+        if re.match(Parser.a_command_pattern, self.current_command):
+            return 'A_COMMAND'
+        elif re.match(Parser.l_command_pattern, self.current_command):
+            return 'L_COMMAND'
+        elif re.match(Parser.c_command_pattern, self.current_command):
+            return 'C_COMMAND'
+        else:
+            return 'UNKNOWN_COMMAND'
+
+    def symbol(self):
+        '''
+        返回当前命令的符号或十进制数
+        仅当commandType()为A_COMMAND或L_COMMAND时调用
+        '''
+        pass
+
+    def dest(self):
+        '''
+        返回当前C_COMMAND的dest部分
+        仅当commandType()为C_COMMAND时调用
+        '''
+        if self.commandType() == 'C_COMMAND':
+            match = re.match(Parser.c_command_pattern, self.current_command)
+            if match:
+                return match.group(1) if match.group(1) else 'null'
+        return None
+
+    def comp(self):
+        '''
+        返回当前C_COMMAND的comp部分
+        仅当commandType()为C_COMMAND时调用
+        '''
+        if self.commandType() == 'C_COMMAND':
+            match = re.match(Parser.c_command_pattern, self.current_command)
+            if match:
+                return match.group(2) if match.group(2) else 'null'
+        return None
+
+    def jump(self):
+        '''
+        返回当前C_COMMAND的jump部分
+        仅当commandType()为C_COMMAND时调用
+        '''
+        if self.commandType() == 'C_COMMAND':
+            match = re.match(Parser.c_command_pattern, self.current_command)
+            if match:
+                return match.group(3) if match.group(3) else 'null'
+        return None
+
+    def __del__(self):
+        self.file.close()
